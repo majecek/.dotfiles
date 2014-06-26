ef6136d95ce1daa375fb4700edab55bf4c4bc452
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-06-27 00:13:42 +0200

    symlink

--- install-deps.sh
+++ install-deps.sh
@@ -6,7 +6,7 @@ makeLink () {
     SOURCE_FILE="${DOTFILES}${${TARGET_FILE}#${HOME}}"
     [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 1
     echo -e "\t${TARGET_FILE} -> ${SOURCE_FILE}"
-    rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
+#    rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
 }
 
 
@@ -25,7 +25,7 @@ curl -L -s get.jenv.io | bash
 
 # update z from repository
 # https://github.com/rupa/z
-git -C z pull
+#git -C z pull
 
 DOTFILES=${HOME}/.dotfiles
 OH_MY_ZSH=~/.oh-my-zsh
@@ -33,9 +33,10 @@ OH_MY_ZSH=~/.oh-my-zsh
 
 
 echo "Setting up Zsh and Oh-My-Zsh custom additions"
-makeLink ${HOME}/z.sh
+#makeLink ${HOME}/z.sh
 mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-settings
-makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+ln -s ${DOTFILES}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+#makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
 
 
 
