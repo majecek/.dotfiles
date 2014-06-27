dacfc0776e1883f27a1e22e55dbdc54ccf63240a
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-06-27 09:04:06 +0200

    fixed symlinks

--- install-deps.sh
+++ install-deps.sh
@@ -1,23 +1,20 @@
-# up to you (me) if you want to run this as a file or copy paste at your leisure
-sudo -v
+#!/bin/zsh
 
 makeLink () {
     TARGET_FILE=$1
     SOURCE_FILE="${DOTFILES}${${TARGET_FILE}#${HOME}}"
     [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 1
-    echo -e "\t${TARGET_FILE} -> ${SOURCE_FILE}"
-#    rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
+    echo -e "\t${SOURCE_FILE} -> ${TARGET_FILE}"
+    rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
 }
 
+# up to you (me) if you want to run this as a file or copy paste at your leisure
+sudo -v
 
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-
-# https://github.com/robbyrussell/oh-my-zsh
-#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-#chsh -s /usr/local/bin/zsh
-
+# OH_MY_ZSH
 curl -L http://install.ohmyz.sh | sh
 
 # jenv.io
@@ -31,14 +28,10 @@ DOTFILES=${HOME}/.dotfiles
 OH_MY_ZSH=${HOME}/.oh-my-zsh
 [ ! -d ${DOTFILES} ] && echo "${DOTFILES} does not exist, exiting" && exit 1
 
-
 echo "Setting up Zsh and Oh-My-Zsh custom additions"
-#makeLink ${HOME}/z.sh
 mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-settings
-ln -s ${DOTFILES}/.oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh ${OH_MY_ZSH}/custom/plugins/oh-my-settings/
-ln -s ${DOTFILES}/.zshrc ${HOME}/
-
-#makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+makeLink ${HOME}/.zshrc 
+makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
 
 
 
