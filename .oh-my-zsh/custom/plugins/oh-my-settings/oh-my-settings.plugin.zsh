3c307947c71e7894edf03a67c4357408ec5180b9
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-11-06 14:28:53 +0100

    changed symlinks

--- .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
@@ -1,1 +1,5 @@
-/Users/marek/.oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+alias ,ll='ls -alF'
+alias ,dv='cd /Users/marek/work/src/vendavo'
+alias ,dg='cd /Users/marek/git'
+alias ,dt='cd /Users/marek/tmp'
+alias ,openports="sudo lsof -i -P | grep -i 'listen'"
