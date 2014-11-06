5d657583b439a774b2e7d1e3bc11d3473aaec08b
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-11-06 14:24:37 +0100

    added few aliases

--- .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
@@ -1,2 +1,5 @@
-
 alias ,ll='ls -alF'
+alias ,dv='cd /Users/marek/work/src/vendavo'
+alias ,dg='cd /Users/marek/git'
+alias ,dt='cd /Users/marek/tmp'
+alias ,openports="sudo lsof -i -P | grep -i 'listen'"
