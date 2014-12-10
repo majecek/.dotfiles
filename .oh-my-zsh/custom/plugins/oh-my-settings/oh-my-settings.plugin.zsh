bc6a200303dec14565e3c3408dd3531c9e851794
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-12-10 01:10:13 +0100

    added docker alias

--- .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
@@ -3,3 +3,4 @@ alias ,dv='cd /Users/marek/work/src/vendavo'
 alias ,dg='cd /Users/marek/git'
 alias ,dt='cd /Users/marek/tmp'
 alias ,openports="sudo lsof -i -P | grep -i 'listen'"
+alias ,dock="ssh root@docker"
