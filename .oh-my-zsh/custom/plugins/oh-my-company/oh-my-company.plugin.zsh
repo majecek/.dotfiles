6e99d8688413344fc41886ed1ee7c60ab7374a36
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-07-15 12:21:00 +0200

    twiddle jboss usefull info

--- .oh-my-zsh/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
@@ -1,17 +1,13 @@
 _dev1() { ./vv81 dbuser new && ./vv81 dev1 -DskipTests=true  && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug && sleep 65 && ./vv81 dev2 && say "done done done" }
 alias ,vall=_dev1
 
-
-_cleanAndBuild() {
- cd '/Users/marek/work/src/vendavo' && rm -Rf ./classes ./scratch ./web ./jboss_home && ./vv81 dev1-noschema skipTests=true && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug
-}
+_cleanAndBuild() { cd '/Users/marek/work/src/vendavo' && rm -Rf ./classes ./scratch ./web ./jboss_home && ./vv81 dev1-noschema skipTests=true && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug }
 alias ,vCleanBuild=_cleanAndBuild
 
 ,vjbossHomeOnly() {./vv81 jboss-gen-startup-scripts}
 
 ,videv() {./vv81 idev-noschema && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug }
 
-,vjbossLogging() {
- cd '/Users/marek/.jenv/candidates/jboss/current/bin' && ./twiddle.sh -s localhost:1099 -u admin -p admin invoke "jboss.system:service=Logging,type=Log4jService" setLoggerLevel $1 "INFO"
-}
+,vjbossLogging() {  cd '/Users/marek/.jenv/candidates/jboss/current/bin' && ./twiddle.sh -s localhost:1099 -u admin -p admin invoke "jboss.system:service=Logging,type=Log4jService" setLoggerLevel $1 "INFO" }
 
+,jbossUsefullInfo() { cd '/Users/marek/.jenv/candidates/jboss/current/bin' && ./twiddle.sh -s 127.0.0.1:1099 get jboss.system:type=ServerInfo FreeMemory MaxMemory TotalMemory ActiveThreadCount }
