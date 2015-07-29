37273a5b4999a3cbe28e917a836bf71d04becc5c
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-07-29 23:04:35 +0200

    some changes

--- .oh-my-zsh/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
@@ -1,8 +1,12 @@
-_dev1() { ./vv81 dbuser new && ./vv81 dev1 -DskipTests=true  && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug && sleep 65 && ./vv81 dev2 && say "done done done" }
-alias ,vall=_dev1
+,vall() { 
+./vv81 dbuser new 
+./vv81 dev1 -DskipTests=true  
+./jboss_home/server/vendavo-dev/bin/startJboss.sh debug & 
+ sleep 60 
+./vv81 dev2 
+say "done done done" }
 
-_cleanAndBuild() { cd '/Users/marek/work/src/vendavo' && rm -Rf ./classes ./scratch ./web ./jboss_home && ./vv81 dev1-noschema skipTests=true && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug }
-alias ,vCleanBuild=_cleanAndBuild
+,vcleanAndBuild() { cd '/Users/marek/work/src/vendavo' && rm -Rf ./classes ./scratch ./web ./jboss_home && ./vv81 dev1-noschema skipTests=true && ./jboss_home/server/vendavo-dev/bin/startJboss.sh debug }
 
 ,vjbossHomeOnly() {./vv81 jboss-gen-startup-scripts}
 
