8aa60f9e4a8ebd65ce446f327eee9881322d1c59
Marek Chowaniok <marek.chowaniok@gmail.com>
2016-11-08 09:46:47 +0100

    Updates

--- .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
@@ -4,3 +4,7 @@ alias ,dg='cd /Users/marek/git'
 alias ,dt='cd /Users/marek/tmp'
 alias ,openports="sudo lsof -i -P | grep -i 'listen'"
 alias ,dock="ssh root@docker"
+
+nmap(){
+	docker run --rm --net=host gvangool/nmap "$@"
+}
