f861729c8db9b11987104030e0880229bd779e51
Marek Chowaniok <marek.chowaniok@gmail.com>
2018-10-03 11:51:20 +0200

    dotfiles changes

--- .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
@@ -7,6 +7,11 @@ alias ,dock="ssh root@docker"
 alias ,gitupdater2="find . -name .git -type d | xargs -n1 -P4 -I% git --git-dir=% --work-tree=%/.. fetch --all --recurse-submodules"
 alias ,gitupdater="mr "
 
+alias ,wifiscanarea="sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s"
+alias ,wificurrentap="sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo"
+alias ,wifisniff="sudo /usr/libexec/airportd en0 sniff"
+
+
 nmap(){
 	docker run --rm --net=host gvangool/nmap "$@"
 }
